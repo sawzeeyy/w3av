867a155a570a47d061b1a36c360530f1cfb48ab9
Shuaib Oladigbolu <saoladigbolu@gmail.com>
2024-07-17 22:32:24 +0100

    rewrite and rename process_comment_status to remove_comment_delimiter

--- weav/core/comment.py
+++ weav/core/comment.py
@@ -1,13 +1,43 @@
-def process_comments_status(node):
-    node_text = node.text.decode('UTF-8')
+import re
+
+
+def remove_comment_delimiter(text):
     comment_removed = False
+    text = text.strip()
+    current_length = len(text)
+    pattern = re.compile(
+        r'^\/\/'                      # Start with double slash
+        r'(?:'                        # Start non-capturing group for domain/IP
+        r'(?:[a-zA-Z0-9-]+\.)+'       # Domain name part
+        r'[a-zA-Z]{2,}'               # TLD part
+        r'|'                          # OR
+        r'(?:\d{1,3}\.){3}\d{1,3}'    # IP address part
+        r')'                          # End non-capturing group for domain/IP
+        r'(?::\d{1,5})?'              # Optional port number
+        r'(?:\/[^\s]*)?'              # Optional path
+        r'$',                         # End of string
+        re.VERBOSE                    # Ignore space and comments
+    )
 
-    if node_text.startswith('/*'):
+    while text.startswith('/*'):
+        text = text[2:].strip()
+
+    while text.endswith('*/'):
+        text = text[:-2].strip()
+
+    while text.startswith('//') and not pattern.match(text):
+        text = text[2:].strip()
+
+    while text.startswith('/ '):
+        text = text[2:].strip()
+
+    while text.endswith(' /'):
+        text = text[:-2].strip()
+
+    if len(text) == 0:
+        text = None
+
+    if current_length > len(text):
         comment_removed = True
-        node_text = node_text.strip('/*/')
 
-    while node_text.startswith('//'):
-        comment_removed = True
-        node_text = node_text[2:].strip()
-
-    return node_text, comment_removed
+    return text, comment_removed
